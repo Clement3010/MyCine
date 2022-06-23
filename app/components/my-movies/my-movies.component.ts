b4d446efc22cd3f9fa0277be0c7209d0784cbd74
Clement3010 <73697560+Clement3010@users.noreply.github.com>
2022-06-23 12:03:20 +0200

    Add files via upload

---
+++ app/components/my-movies/my-movies.component.ts
@@ -0,0 +1,31 @@
+import { Component, EventEmitter, OnInit, Output} from '@angular/core';
+import { Movie } from 'src/app/classes/movie';
+import { MovieService } from 'src/app/services/movie.service';
+
+@Component({
+  selector: 'app-my-movies',
+  templateUrl: './my-movies.component.html',
+  styleUrls: ['./my-movies.component.css']
+})
+export class MyMoviesComponent implements OnInit {
+
+  constructor(private movieService: MovieService) { }
+
+  @Output() editMovieEvent = new EventEmitter<Movie>();
+  
+  movies!: Array<Movie>;
+  displayedColumns: string[] = ['title', 'synopsis', 'rating'];
+
+  ngOnInit(): void {
+    this.movieService.getAllMovies().subscribe(response => {
+      this.movies = response;
+    });;
+  }
+ 
+  editMovie(movie: Movie) {
+    this.editMovieEvent.emit(movie);
+  }
+
+  //@Input()  movies: Array<Movie> = [];
+
+}
